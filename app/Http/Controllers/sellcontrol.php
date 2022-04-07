61bd391456b1d1ca9a8cce06048ca24b74bfaf68
abithputhanpura <abithp764@gmail.com>
2022-04-07 19:07:26 +0530

    v1.7v

--- app/Http/Controllers/sellcontrol.php
+++ app/Http/Controllers/sellcontrol.php
@@ -10,12 +10,12 @@ class sellcontrol extends Controller
     public function sell()
         {
 
-            $apple = DB::table('smartphones')->where('brand', 'Apple')->get();
-            $samsung = DB::table('smartphones')->where('brand', 'Samsung')->get();
-            $mi = DB::table('smartphones')->where('brand', 'mi')->get();
-            $others = DB::table('smartphones')->where('brand', 'others')->get();
+            $apple = DB::table('smartphone')->where('brand', 'Apple')->get();
+            $samsung = DB::table('smartphone')->where('brand', 'Samsung')->get();
+            $mi = DB::table('smartphone')->where('brand', 'mi')->get();
+            $others = DB::table('smartphone')->where('brand', 'others')->get();
 
-            $user=DB::table('users')->get();
+            $user=DB::table('users')->get('name');
      
             return view('sell', ['mobile1' => $apple], ['mobile2' =>$samsung],['mobile3' =>$mi],['mobile4' =>$others],['user'=>$user]);
         }
