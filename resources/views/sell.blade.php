8f05d21a3f84efc76edc2a3a49d1d1accd8ee984
abithputhanpura <abithp764@gmail.com>
2022-04-10 00:35:35 +0530

    v2.2

--- resources/views/sell.blade.php
+++ resources/views/sell.blade.php
@@ -2,6 +2,8 @@
   @section('content')
   <html>
   <head>
+
+
     <title>sell.me</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
@@ -11,31 +13,54 @@
 </div>
 <form  method="POST" action="{{route('sell.store')}}">
   @csrf
-    <div class="container py-2" style="font-family: 'Montserrat', sans-serif;">
-	  <div class="row">
-    <div class="col">
-    <div class="row row-cols-2">
-	  <div class="col">Brand</div>
-    <div class="col">   
-    <select id="brand" name="brand" class="form-select" aria-label="Default select example" >
-    <option >select brand</option>
-    <option value="Apple">Apple</option> 
-    <option value="Samsung" >Samsung</option> 
-    <option value="mi" selected="selected">mi</option> 
-    <option value="others">other</option> 
-    </select>
 
+  <div class="container">
+    <div class="panel panel-default">
+      <div class="panel-heading">Select State and get bellow Related City</div>
+      <div class="panel-body">
+            <div class="form-group">
+                <label for="title">Select State:</label>
+                <select name="state" class="form-control" style="width:350px">
+                    <option value="">--- Select State ---</option>
+                    @foreach ($states as $key) 
+                        <option value="{{ $key->brand_name }}">{{ $key->brand_name  }}</option>
+                     @endforeach
+                    <option value="">phones</option>
+                  </select>
+            </div>
+            <div class="form-group">
+                <label for="title">Select City:</label>
+                <select name="city" class="form-control" style="width:350px">
+                </select>
+            </div>
+      </div>
+    </div>
 </div>
-<div class="col g-4">Model</div>
-   <div class="col g-4"><select name="model" class="form-select" aria-label="Default select example">
-    <option>select Model</option> 
-   
-     @foreach ($mobile as $item)
-    <option value="{!!$item->model!!}">{{$item->model}}</option> 
-      @endforeach  
-     
-  </select>
-  </div>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('select[name="state"]').on('change', function() {
+            var stateID = $(this).val();
+            if(stateID) {
+                $.ajax({
+                    url: '/sell/ajax/'+stateID,
+                    type: "GET",
+                    dataType: "json",
+                    success:function(data) {
+                        
+                        $('select[name="city"]').empty();
+                        $.each(data, function(key, value) {
+                            $('select[name="city"]').append('<option value="'+ key +'">'+ value +'</option>');
+                        });
+                    }
+                });
+            }else{
+                $('select[name="city"]').empty();
+            }
+        });
+    });
+</script>
+
 
   <div class="col g-4">Varient</div>
     <div class="col g-4">
@@ -44,7 +69,7 @@
     <option value="4gb">4gb</option> 
     <option value="6gb">6gb</option> 
     </select></div>
-	</div>
+	</div> 
 	<div class="g-4 py-2"><h4>Extra Credit After Analysis Please Check Below Applicable Conditions:</h4></span></div>
 	<div class="col-lg">
     <div class="form-check">
@@ -69,5 +94,6 @@
 </form>
   </div>
 </div>
+
 </body>
 @endsection
