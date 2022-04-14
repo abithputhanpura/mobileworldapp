5bcba00d568965a0c95e00a244f6a45657bc1d86
abithputhanpura <abithp764@gmail.com>
2022-04-14 14:36:33 +0530

    v6

--- app/Http/Controllers/DynamicDependent.php
+++ app/Http/Controllers/DynamicDependent.php
@@ -11,7 +11,7 @@ class DynamicDependent extends Controller
     function index()
     {
      $country_list = DB::table('phonedetails')
-         ->groupBy('brand')
+         ->groupBy('id','brand','model')
          ->get();
      return view('sell')->with('country_list', $country_list);
     }
